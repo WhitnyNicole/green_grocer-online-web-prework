6e53345c8af297f24dc8e71bb51f50d0299df036
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 02:27:02 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -33,9 +33,22 @@ def apply_coupons(cart:[], coupons:[])
   result
 end	
 
-def apply_clearance(cart)
-  
-end
+
+def apply_clearance(cart:[])
+  clearance_cart = {}
+  # code here	  # code here
+  cart.each do |food, info|
+    clearance_cart[food] = {}
+    if info[:clearance] == true
+      clearance_cart[food][:price] = info[:price] * 4 / 5
+    else
+      clearance_cart[food][:price] = info[:price]
+    end
+    clearance_cart[food][:clearance] = info[:clearance]
+    clearance_cart[food][:count] = info[:count]
+  end
+  clearance_cart
+end	
 
 def checkout(cart, coupons)
 
