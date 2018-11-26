6647613c36dbcfd750541fd2f93c4dc8ed80149c
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 02:26:18 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,18 @@
-def consolidate_cart(cart)
-
-end
+def consolidate_cart(cart:[])
+  result = {}
+  # code here	  # code here
+  cart.each_with_index do |item, i|
+    item.each do |food, info|
+      if result[food]
+        result[food][:count] += 1
+      else
+        result[food] = info
+        result[food][:count] = 1
+      end
+    end
+  end
+  result
+end	
 
 def apply_coupons(cart, coupons)
 
