f3244433c6460ff7f1b8ad1692d1f3600f49260e
Whitny Edwards <wne4e2@gmail.com>
2018-11-26 02:24:01 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,30 +1,14 @@
-def consolidate_cart(cart: [])
+def consolidate_cart(cart)
 
 end
 
-def apply_coupons(cart: [], coupons: [])
- coupons_applied = {}
-  coupons.each do |coupon|
-    if cart.key?(coupon[:item])
-      coupon_count = 0
-      until coupon[:num] > cart[coupon[:item]][:count]
-        cart[coupon[:item]][:count] -= coupon[:num]
-        cart["#{coupon[:item]} W/COUPON"] = {price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: coupon_count += 1}
-      end
-    end
-  end
-  cart.merge(coupons_applied)
+def apply_coupons(cart, coupons)
+
 end 
 
-def apply_clearance(cart:[])
-  cart.each {|grocery,value| cart[grocery][:price] = (cart[grocery][:price] * 0.8).round(2) if cart[grocery][:clearance] }
+def apply_clearance(cart)
 end
 
-def checkout(cart: [], coupons: [])
- cart = consolidate_cart(cart: cart)
-  cart = apply_coupons(cart: cart, coupons: coupons)
-  cart = apply_clearance(cart: cart)
-  total = 0
-  cart.each {|grocery,value| total += (cart[grocery][:price] * cart[grocery][:count]) if cart[grocery][:count] > 0}
-  total > 100 ? (total*0.9).round(2) : total
+def checkout(cart, coupons)
+
 end
